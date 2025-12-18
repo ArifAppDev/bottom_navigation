a26d0d6ad9a4d803d4e435aee5e896d9a5005a7e
ArifAppDev <arifgt77@gmail.com>
2025-12-18 11:43:28 +0600

    new commit

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:practice_app/asset_helpers/app_colors.dart';
 import 'package:practice_app/asset_helpers/app_fonts.dart';
+import 'package:practice_app/helpers/ui_helpers.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -22,7 +24,58 @@ class ProfileScreen extends StatelessWidget {
         elevation: 0,
         backgroundColor: AppColor.c4db8ff,
       ),
-      body: Center(child: Text("Profile screen")),
+      body: Column(
+        children: [
+          UIHelper.verticalSpace(40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    child: CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    right: 10,
+                    child: Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: AppColor.c4db8ff,
+                        borderRadius: BorderRadius.circular(80),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          color: AppColor.cffffff,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(children: [
+
+                
+
+
+
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
